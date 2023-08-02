1fc15579e4dc8c1e6709dd4990c0f62cbcfb4c03
priyanka-gite <pgite7@gmail.com>
2023-08-02 23:01:38 +0200

    first quiz done

--- src/novi/higherlower/HigherLowerGame.java
+++ src/novi/higherlower/HigherLowerGame.java
@@ -1,5 +1,6 @@
 package novi.higherlower;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class HigherLowerGame {
@@ -12,16 +13,26 @@ public class HigherLowerGame {
 
     public void playGame() {
         gameIsRunning = true;
+        Random random = new Random();
+        int number = random.nextInt(101);
+        System.out.println(number); //remove later
 
-        // Here you should generate the number to guess
-
+        int totalTurns = 0;
         while (gameIsRunning) {
+            totalTurns = totalTurns + 1;
             System.out.println("Make a guess");
 
-            var input = inputScanner.nextLine();
+            var input = inputScanner.nextInt();//blocking call
             System.out.printf("You typed: %s\n", input);
 
-            // Check if the guess is correct, if so end the game
+            if (input == number) {
+                gameIsRunning = false;
+                System.out.printf("You guessed it correctly in : %s turns", totalTurns);
+            } else if (input > number) {
+                System.out.println("That number is too high");
+            } else if (input < number) {
+                System.out.println("That number is too low");
+            }
         }
     }
 }
